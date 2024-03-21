f1cddf2f17a07efe2d4c3466d53973b03f704fc1
obedhan <obedienthan@gmail.com>
2024-03-21 17:39:35 -0500

    home page fixes

--- Project/my-app/app/home.js
+++ Project/my-app/app/home.js
@@ -3,14 +3,15 @@ import Image from 'next/image';
 
 export default function Home() {
 
-    return (
+  return (
+    <div>
       <div className='d-flex align-items-center vh-100'>
-      <Image
+        <Image
           src={backgroundImage}
           layout='fill'
           priority={false}
           alt="background"
-          style={{zIndex:-5}}
+          style={{ zIndex: -5 }}
         />
         <div className="container text-center">
           <div className="row m-3">
@@ -21,27 +22,52 @@ export default function Home() {
           <div className="row m-3 mb-0">
             <div className="col">
               <div className="form-floating mb-3">
-                <input type="email" className="form-control" id="floatingFrom" placeholder="from" />
+                <input type="text" className="form-control" id="floatingFrom" placeholder="from" />
                 <label htmlFor="floatingFrom">From</label>
               </div>
             </div>
             <div className="col">
               <div className="form-floating">
-                <input type="email" className="form-control" id="floatingTo" placeholder="to" />
+                <input type="text" className="form-control" id="floatingTo" placeholder="to" />
                 <label htmlFor="floatingTo">To</label>
               </div>
             </div>
           </div>
-          
+
           <div className="row">
             <div className="col">
               <div className='form-floating'>
-              <button class="btn btn-primary" type="submit">GO</button>
+                <button class="btn btn-primary" type="submit">GO</button>
               </div>
             </div>
           </div>
 
         </div>
       </div>
-    );
-  }
+      <div className='container'>
+        <div className='row align-items-center'>
+          <div className='col'>
+            <div class="card">
+              <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+          <div className='col'>
+          <div class="card">
+              <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
