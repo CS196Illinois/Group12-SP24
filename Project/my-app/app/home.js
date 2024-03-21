23b0b1418fb99afb0eb060ef476898f6099df058
obedhan <obedienthan@gmail.com>
2024-03-21 17:09:34 -0500

    home prototype complete

--- Project/my-app/app/home.js
+++ Project/my-app/app/home.js
@@ -4,30 +4,44 @@ import Image from 'next/image';
 export default function Home() {
 
     return (
-      <main>
-        <div class='container d-flex align-item-center'>
-          <Image
-            src={backgroundImage}
-            layout='fill'
-            priority={false}
-            alt="background"
-          />
-          <div class='row-auto'>
-            <div class='col-auto'>
-              <div class="card">
-                <div class="card-header">
-                  Featured
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+      <div className='d-flex align-items-center vh-100'>
+      <Image
+          src={backgroundImage}
+          layout='fill'
+          priority={false}
+          alt="background"
+          style={{zIndex:-5}}
+        />
+        <div className="container text-center">
+          <div className="row m-3">
+            <div className="col text-light">
+              <h4 className="text-center duru-sans-regular">Our mission is to simplify travel by providing an intuitive app for purchasing tickets and navigating journeys. We aim to make travel accessible and stress-free for all, revolutionizing the way people explore the world.</h4>
+            </div>
+          </div>
+          <div className="row m-3 mb-0">
+            <div className="col">
+              <div className="form-floating mb-3">
+                <input type="email" className="form-control" id="floatingFrom" placeholder="from" />
+                <label htmlFor="floatingFrom">From</label>
+              </div>
+            </div>
+            <div className="col">
+              <div className="form-floating">
+                <input type="email" className="form-control" id="floatingTo" placeholder="to" />
+                <label htmlFor="floatingTo">To</label>
               </div>
             </div>
           </div>
+          
+          <div className="row">
+            <div className="col">
+              <div className='form-floating'>
+              <button class="btn btn-primary" type="submit">GO</button>
+              </div>
+            </div>
+          </div>
+
         </div>
-  
-      </main>
+      </div>
     );
   }
--- Project/my-app/app/page.js
+++ Project/my-app/app/page.js
@@ -2,55 +2,14 @@
 import Image from 'next/image';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import backgroundImage from './3L2A7490.jpg';
+import Home from './home.js'
 
-export default function Home() {
+export default function Page() {
 
   return (
-    // 
+
     <main>
-      <div class='container d-flex align-item-center'>
-        <Image
-          src={backgroundImage}
-          layout='fill'
-          priority={true}
-          alt="background"
-        />
-      </div>
-
-      <div class="container d-flex justify-content-center align-items-center vh-100">
-
-
-        <div class="card">
-          <div class="card-body">
-            <h6 class="card-title">Our mission is to simplify travel by providing an intuitive app for purchasing tickets and navigating journeys. We aim to make travel accessible and stress-free for all, revolutionizing the way people explore the world. </h6>
-
-          </div>
-        </div>
-
-
-
-        <div class="container d-flex col ">
-
-
-          <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="floatingFrom" placeholder="from" />
-            <label for="floatingFrom">From</label>
-          </div>
-
-
-
-          <div class="form-floating">
-            <input type="email" class="form-control" id="floatingTo" placeholder="to" />
-            <label for="floatingTo">To</label>
-          </div>
-
-        </div>
-
-
-
-      </div>
-
-
+      <Home></Home>
     </main>
   );
 }
